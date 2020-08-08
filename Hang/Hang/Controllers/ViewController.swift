faa104e8890cb5dde06994a6fd0a53c07ba4d4e6
Devfactori II <msohaib@devfactori.com>
2020-08-08 15:42:41 +0500

    Project setup

---
+++ Hang/Hang/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hang
+//
+//  Created by Devfactori II on 8/8/20.
+//  Copyright © 2020 Hang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
